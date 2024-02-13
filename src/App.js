5a5a20d3bc82b7f8d42e93209aa63d0012c17ac9
Vaidehi Shrivastava <vaidehishrivastava09@gmail.com>
2024-02-13 14:48:33 +0530

    CSS file and Landing page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import InventoryStats from "./pages/InventoryStats";
+import "./App.css"
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary:
+    {
+      main:"#eafc86"
+    }
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <InventoryStats />
+    </ThemeProvider>
   );
 }
 
