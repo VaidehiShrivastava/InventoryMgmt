9efb4ac75a87819c66471599d15b18ce76c2e4d6
Vaidehi Shrivastava <vaidehishrivastava09@gmail.com>
2024-02-13 14:57:01 +0530

    Status codes and mssgs added

---
+++ src/constants/StatusCodes.js
@@ -0,0 +1,15 @@
+const CODE = {
+    SERVER_ERROR: -2,
+    Loading: -1,
+    SUCCESS : 200,
+    ERROR: 404,
+    
+}
+const CODEMSG = {
+    SERVER_ERROR: "Oops! Something went wrong from server side",
+    Loading: "Loading...",
+    SUCCESS : "Loaded",
+    ERROR: "No data found",
+}
+
+export {CODE,CODEMSG}
